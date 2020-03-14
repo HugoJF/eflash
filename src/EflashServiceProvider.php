41e14fd18daf42287b298e2e3ae1398325fc8793
Hugo Jeller <hugo_jeller@hotmail.com>
2020-03-14 21:56:40 +0000

    Added missing file

--- src/EflashServiceProvider.php
+++ src/EflashServiceProvider.php
@@ -11,37 +11,7 @@ class EflashServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'eflash');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'eflash');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-//        if ($this->app->runningInConsole()) {
-//            $this->publishes([
-//                __DIR__.'/../config/config.php' => config_path('eflash.php'),
-//            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/eflash'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/eflash'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/eflash'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
-//        }
+        //
     }
 
     /**
@@ -49,9 +19,6 @@ class EflashServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
-//        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'eflash');
-
         // Register the main class to use with the facade
         $this->app->singleton('eflash', function () {
             return new Eflash;
